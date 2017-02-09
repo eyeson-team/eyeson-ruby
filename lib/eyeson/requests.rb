c91e4927331f2d1ec61700459cb65a875114f162
Michael Maier <michael.maier@visocon.com>
2017-02-09 13:02:30 +0100

    remove debugging

--- lib/eyeson/requests.rb
+++ lib/eyeson/requests.rb
@@ -17,9 +17,7 @@ module Eyeson
     req['Authorization'] = configuration.api_key
 
     res = http.request(req)
-    Rails.logger.info "REQUESTING to #{uri} with #{req.inspect}"
-    Rails.logger.info "RESPONSE: #{res.inspect}"
-    #return unless res.body.present?
+    return unless res.body.present?
     JSON.parse(res.body)
   end
   module_function :request
