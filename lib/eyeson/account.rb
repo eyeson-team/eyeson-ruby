9bafdbc31b39f236eb48f9807e4173422dea71e8
Michael Maier <michael.maier@visocon.com>
2017-07-14 12:07:27 +0200

    accounts via internal api

--- lib/eyeson/account.rb
+++ lib/eyeson/account.rb
@@ -11,25 +11,14 @@ module Eyeson
     end
 
     def self.find_or_initialize_by(user: {})
-      confirmed = confirmed?(user: user)
+      confirmed = Eyeson.post('/internal/accounts',
+                              user: user)
       confirmation_url = confirmed['create_url'] if confirmed.present?
       Account.new(confirmation_url: confirmation_url)
     end
 
     def new_record?
       @confirmation_url.present?
-    end
-
-    def self.confirmed?(user: {})
-      url = "#{Eyeson.configuration.account_endpoint}/confirmation"
-      req = RestClient::Request.new(
-        method: :get,
-        url: url,
-        headers: { authorization: Eyeson.configuration.account_api_key,
-                   accept: 'application/json',
-                   params: { user: user } }
-      )
-      Eyeson.response_for(req)
     end
   end
 end
--- lib/eyeson/config.rb
+++ lib/eyeson/config.rb
@@ -2,13 +2,10 @@ module Eyeson
   # API configuration class
   class Configuration
     attr_accessor :api_key, :api_endpoint
-    attr_accessor :account_endpoint, :account_api_key
 
     def initialize
-      @api_key           = ''
-      @api_endpoint      = 'https://api.eyeson.team'
-      @account_api_key   = ''
-      @account_endpoint  = 'https://account.eyeson.team/api'
+      @api_key      = ''
+      @api_endpoint = 'https://api.eyeson.team'
     end
   end
 end
