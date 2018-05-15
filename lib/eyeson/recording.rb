674d48180ee1a501539c2ef86f808f54ac2141c8
Christoph Lipautz <christoph@lipautz.org>
2018-05-15 10:11:55 +0200

    Provide recordings delete method

--- lib/eyeson/recording.rb
+++ lib/eyeson/recording.rb
@@ -4,17 +4,22 @@ module Eyeson
     class NotFound < StandardError
     end
 
-    attr_reader :url
+    attr_reader :id, :url
 
-    def initialize(url)
+    def initialize(id, url)
+      @id = id
       @url = url
     end
 
     def self.find(recording_id)
-      response  = Eyeson.get("/recordings/#{recording_id}")
+      response  = Eyeson.get "/recordings/#{recording_id}"
       links     = response['links']
       raise NotFound if links.nil?
-      Recording.new(links['download'])
+      Recording.new recording_id, links['download']
+    end
+
+    def delete
+      Eyeson.delete "/recordings/#{id}"
     end
   end
 end
--- spec/lib/recording_spec.rb
+++ spec/lib/recording_spec.rb
@@ -16,4 +16,9 @@ RSpec.describe Eyeson::Recording, type: :class do
     Eyeson.expects(:get).returns({})
     expect { Eyeson::Recording.find('123') }.to raise_error(Eyeson::Recording::NotFound)
   end
+
+  it 'should provide method to delete recording' do
+    Eyeson.expects(:delete).with "/recordings/123"
+    Eyeson::Recording.new(123, '').delete
+  end
 end
